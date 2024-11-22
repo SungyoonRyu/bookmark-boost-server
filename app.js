85c5cecf1d6ac1f00144370fd735a26a6d4fda74
SungyoonRyu <rara0620@naver.com>
2024-11-22 14:11:31 +0900

    feat: add cors and body setting

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const cors = require('cors');
+
+app.use(cors());
+
+// to get body data in post requests
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// connect routes
+const bookmarkRoutes = require('./routes/api_v1/bookmark');
+app.use('/api', bookmarkRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
